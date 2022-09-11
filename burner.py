a3e9baf0ad03f3e6d1e0abca9516978fe71da077
Alberto Pinardi <alberto.pinardi@criptalia.it>
2022-09-11 18:20:10 +0200

    Monitor some ds18b20 sensors to get temperature reference

--- burner.py
+++ burner.py
@@ -1,12 +1,16 @@
-from prometheus_client import start_http_server, Summary
+from prometheus_client import start_http_server, Summary, Gauge
 import time
 import RPi.GPIO as GPIO
 import logging
 import signal
 import sys
-
+from w1thermsensor import W1ThermSensor, Sensor
 
 BURNER_TIME = Summary('burner_seconds', 'Time spent burning and power cycling since last reset')
+OUTDOOR_TEMP = Gauge('outdoor_temperature','Outdoor Temperature near the house at 2.5mt from the ground')
+OUTDOOR_HWID = "3c01f0961f04"
+BOILER_TEMP = Gauge('boiler_temperature','Boiler water Temperature As-Is')
+BOILER_HWID = "3c01f09606e5"
 BUTTON_GPIO = 16
 
 
@@ -35,7 +39,17 @@ def burner_on(*args):
         time.sleep(1)
     stop = time.perf_counter_ns()
     duration = stop - start
-    logger.debug(f"Burner on for {duration / 1e9 } seconds")        
+    logger.debug(f"Burner on for {duration / 1e9 } seconds")
+
+
+def gather_temperature(sensor_id, *args):
+    """Read temperature from Sensors"""
+    logger.debug(f"gather_temperature called: {args}")
+    # Issue the conv_temp command
+    sensor = W1ThermSensor(sensor_type=Sensor.DS18B20, sensor_id=sensor_id)
+    temperature_in_celsius = sensor.get_temperature()
+    # return all values from the bus
+    return temperature_in_celsius
 
 
 def signal_handler(sig, frame):
@@ -52,7 +66,9 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     logger.debug("Add event detector")
+    signal.signal(signal.SIGINT, signal_handler)
+    GPIO.add_event_detect(BUTTON_GPIO, GPIO.RISING, callback=burner_on, bouncetime=1000)
     while True:
-        GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, callback=burner_on, bouncetime=500)
-        signal.signal(signal.SIGINT, signal_handler)
-        signal.pause()
+        BOILER_TEMP.set(gather_temperature(BOILER_HWID))
+        OUTDOOR_TEMP.set(gather_temperature(OUTDOOR_HWID))
+        time.sleep(5)
